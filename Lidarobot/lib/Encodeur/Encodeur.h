fa8d27993f32f0087fe9bc0c0ffc32bfbd8ebdb7
Quentin <Quentin@ASUS_Quent1.com>
2023-11-13 14:51:21 +0100

    Refactored Encodeur class to include new initialization and print functions

--- Lidarobot/lib/Encodeur/Encodeur.h
+++ Lidarobot/lib/Encodeur/Encodeur.h
@@ -10,14 +10,21 @@ class Encodeur {
     ESP32Encoder encoderG;
     long oldPositionD;
     long oldPositionG;
+    int x;
+    int y;
+    int theta;
+    float rayon;
 
   public:
     Encodeur(int pinA, int pinB,int pinC, int pinD);
     void init();
+    void init(int x, int y, int theta);
+    void change_position(int x, int y, int theta);
     int readEncoderD();
     int readEncoderG();
     void reset();
     void odometrie(float* x, float* y, float* theta);
+    void print();
 };
 
 #endif

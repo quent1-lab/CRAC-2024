1d8978fa214a4be2e152aae8b358e6b9010194d6
Quentin <quentin.cunha@outlook.fr>
2023-11-15 20:07:01 +0100

    Add ledc_channel parameter to Moteur constructor and implement smoothMoteur function

--- Lidarobot/lib/Moteur/Moteur.cpp
+++ Lidarobot/lib/Moteur/Moteur.cpp
@@ -17,11 +17,36 @@ void Moteur::init()
     ledcSetup(ledc_channel, 20000, 10);
     ledcAttachPin(pinPWM, ledc_channel);
     ledcWrite(ledc_channel, 0);
-    // Other initialization if needed
 }
 
 void Moteur::setVitesse(int vitesse)
 {
+    /*
+     * Input : vitesse : vitesse du moteur en pourcentage (int)
+     * Output : none
+     * Définition : Cette fonction permet de définir la vitesse du moteur en pourcentage.
+     *             La vitesse est comprise entre -100 et 100.
+     */
+
+    if (vitesse > 100)
+    {
+        vitesse = 100;
+    }
+    else if (vitesse < -100)
+    {
+        vitesse = -100;
+    }
+    if (vitesse < 0)
+    {
+        sens_actuel = true;
+        vitesse = -vitesse;
+    }
+    else
+    {
+        sens_actuel = false;
+    }
+
+    smoothMoteur();
     vitesse_consigne = vitesse;
 }
 
@@ -32,7 +57,7 @@ void Moteur::setSens(bool sens)
 
 void Moteur::smoothMoteur()
 {
-    if (abs(vitesse_actuelle - vitesse_consigne) > 100)
+    if (abs(vitesse_actuelle - vitesse_consigne) >= 100)
     {
         if (millis() > temps_mot + 2)
         {

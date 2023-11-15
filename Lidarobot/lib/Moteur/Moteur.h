6516df0ca0ae13f5eb8e6c302fa5261b43cf5667
Quentin <quentin.cunha@outlook.fr>
2023-11-15 20:07:01 +0100

    Add ledc_channel parameter to Moteur constructor and implement smoothMoteur function

--- Lidarobot/lib/Moteur/Moteur.h
+++ Lidarobot/lib/Moteur/Moteur.h
@@ -7,13 +7,14 @@ class Moteur {
   private:
     int pinSens;
     int pinPWM;
+    int ledc_channel;
     bool sens_actuel;
     int vitesse_actuelle;
     int vitesse_consigne;
     unsigned long temps_mot;
 
   public:
-    Moteur(int pinSens, int pinPWM);
+    Moteur(int pinSens, int pinPWM, int ledc_channel);
     void init();
     void setVitesse(int vitesse);
     void setSens(bool sens);

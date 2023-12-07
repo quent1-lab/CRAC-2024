f5aead52af19c058ed9eb8477486835ae823cfc0
Quentin <quentin.cunha@outlook.fr>
2023-12-07 21:18:39 +0100

    Refactor movement functions in Encodeur class

--- Lidarobot/src/main.cpp
+++ Lidarobot/src/main.cpp
@@ -38,6 +38,12 @@ void testEncodeur(void);
 void testMoteur(void);
 void test_ligne_droite(void);
 
+// Fonction de déplacement
+bool avancer(float distance);
+bool tourner(float angle);
+bool aller_a(float x, float y);
+bool aller_a(float x, float y, float theta);
+
 /*---------------------- Variable des pins de sortis ---------------------------*/
 // Aucune des pins ne sont bien définies, il faut les définir en fonction des branchements
 // Pin des moteurs
@@ -366,4 +372,47 @@ void test_ligne_droite(){
     }
     break;
   }
+}
+
+bool avancer(float distance){
+  /*
+    Input : distance : distance à parcourir en cm (float)
+    Output : none
+    Description:  Cette fonction permet de faire avancer le robot d'une certaine distance
+                  Un asservissement en position est utilisé pour que le robot avance droit
+  */
+
+  // Variables locales
+  float new_x, new_y; // Nouvelle position du robot
+  float distance_parcourue; // Distance parcourue par le robot
+  float distance_restante; // Distance restante à parcourir
+  float vitesse; // Vitesse du robot
+
+  // Initialisation des variables
+  new_x = x + distance * cos(theta);
+  new_y = y + distance * sin(theta);
+
+  // Calcul de la distance restante à parcourir
+  distance_restante = sqrt(pow(new_x - x, 2) + pow(new_y - y, 2));
+
+  // Asservissement en position
+  while(distance_restante > 0.1){
+    // Calcul de la distance parcourue
+    distance_parcourue = sqrt(pow(x - new_x, 2) + pow(y - new_y, 2));
+
+    // Calcul de la distance restante à parcourir
+    distance_restante = sqrt(pow(new_x - x, 2) + pow(new_y - y, 2));
+
+    // Calcul de la vitesse du robot
+    vitesse = 100 * distance_restante / distance_parcourue;
+
+    // Asservissement en vitesse
+    moteurGauche.setVitesse(vitesse);
+    moteurDroit.setVitesse(vitesse);
+
+    // Mise à jour des données
+    mise_a_jour_donnees();
+  }
+
+
 }

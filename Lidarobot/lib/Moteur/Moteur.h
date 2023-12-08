42f4069d14f49a8c8c3d1c4240f6760294dc4067
Quentin <quentin.cunha@outlook.fr>
2023-12-08 21:01:30 +0100

    Add overloaded init() functions to Moteur class

--- Lidarobot/lib/Moteur/Moteur.h
+++ Lidarobot/lib/Moteur/Moteur.h
@@ -5,17 +5,19 @@
 
 class Moteur {
   private:
-    int pinSens;
-    int pinPWM;
-    int ledc_channel;
+    int pinSens, pinPWM, ledc_channel;
     bool sens_actuel;
-    int vitesse_actuelle;
-    int vitesse_consigne;
+    int vitesse_actuelle, vitesse_consigne;
+    int vitesse_max;
     unsigned long temps_mot;
+    float coefficient;
+
 
   public:
     Moteur(int pinSens, int pinPWM, int ledc_channel);
     void init();
+    void init(int vitesse_max);
+    void init(int vitesse_max, float coefficient);
     void setVitesse(int vitesse);
     void setSens(bool sens);
     void smoothMoteur();

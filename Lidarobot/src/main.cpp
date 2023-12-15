923d4c7eb1887feeaebd9f7de41ff559ab0c66a4
Quentin <quentin.cunha@outlook.fr>
2023-12-15 14:03:17 +0100

    Update task delay and motor task priority

--- Lidarobot/src/main.cpp
+++ Lidarobot/src/main.cpp
@@ -127,7 +127,7 @@ void taskMiseAJourDonnees(void *pvParameters) {
     while (1) {
         // Appeler la fonction de mise à jour des données ici
         mise_a_jour_donnees();
-        vTaskDelay(pdMS_TO_TICKS(20));  // Delay de 20ms
+        vTaskDelay(pdMS_TO_TICKS(1));  // Delay de 20ms
     }
 }
 
@@ -135,8 +135,8 @@ void setup()
 {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);// disable brownout detector
 
-  xTaskCreatePinnedToCore(taskCommuniquer, "taskCommuniquer", 4096, NULL, 2, NULL, 0);
-  xTaskCreatePinnedToCore(taskMoteur, "taskMoteur", 4096, NULL, 1, NULL, 0);
+  //xTaskCreatePinnedToCore(taskCommuniquer, "taskCommuniquer", 4096, NULL, 2, NULL, 0);
+  xTaskCreatePinnedToCore(taskMoteur, "taskMoteur", 4096, NULL, 2, NULL, 0);
   xTaskCreatePinnedToCore(taskMiseAJourDonnees, "taskMiseAJourDonnees", 4096, NULL, 1, NULL, 0);
 
   // put your setup code here, to run once:
@@ -164,6 +164,7 @@ void loop()
     if (bt[NOIR].click())
     {
       etat_sys = 2;
+      encodeur.reset();
     }
     break;
   case 1:
@@ -312,6 +313,8 @@ void avancer(float distance){
     moteurGauche.setVitesse(vitesseG);
     moteurDroit.setVitesse(vitesseD);
   }
+    moteurGauche.setVitesse(2);
+    moteurDroit.setVitesse(2);
 }
 
 void tourner(float angle){
@@ -357,6 +360,8 @@ void tourner(float angle){
     moteurDroit.setVitesse(vitesseD);
 
   }
+  moteurGauche.setVitesse(2);
+  moteurDroit.setVitesse(2);
 }
 
 void aller_a(float X, float Y){

385413d5adea2bb5b06bb24376964f3758c8791d
Quentin <quentin.cunha@outlook.fr>
2023-12-31 00:01:36 +0100

    Delete unnecessary binary files

--- baseF303k8/.pio/build/nucleo_f303k8/FrameworkMbedfeatures/frameworks/mbed-greentea-io/mbed_io.d
+++
@@ -1,4 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedfeatures/frameworks/mbed-greentea-io/mbed_io.o: \
- /home/quentin/.platformio/packages/framework-mbed/features/frameworks/mbed-greentea-io/mbed_io.cpp \
- .pio/build/nucleo_f303k8/mbed_config.h \
- /home/quentin/.platformio/packages/framework-mbed/platform/include/platform/mbed_retarget.h

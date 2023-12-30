477002e89942d7e212092f0ec98af4449a563a0c
Quentin <Quent@Quent1_HP>
2023-12-30 02:03:30 +0100

    Fix LidarScanner stop method

--- Lidar/TestLidar.py
+++ Lidar/TestLidar.py
@@ -1,47 +1,56 @@
-import os
-from math import cos, sin, pi, floor
 import pygame
+from math import cos, sin, pi, floor
 from rplidar import RPLidar
 
-# Set up pygame and the display
+# Initialisation de Pygame et de l'affichage
 pygame.init()
-lcd = pygame.display.set_mode((320,240))
+lcd = pygame.display.set_mode((320, 240))
 pygame.mouse.set_visible(False)
-lcd.fill((0,0,0))
+lcd.fill((0, 0, 0))
 pygame.display.update()
-# Setup the RPLidar
-PORT_NAME = 'COM8'
+
+# Configuration du lidar
+PORT_NAME = 'COM5'  # À modifier en fonction du port utilisé
 lidar = RPLidar(PORT_NAME)
-# used to scale data to fit on the screen
+
+# Utilisé pour mettre à l'échelle les données pour les afficher à l'écran
 max_distance = 0
-#pylint: disable=redefined-outer-name,global-statement
+
+# Cette directive permet d'utiliser la variable globale max_distance
+# pylint: disable=redefined-outer-name,global-statement
 
 def process_data(data):
     global max_distance
-    lcd.fill((0,0,0))
+    lcd.fill((0, 0, 0))
     for angle in range(360):
         distance = data[angle]
-        if distance > 0: # ignore initially ungathered data points
+        if distance > 0:  # Ignorer les points de données initialement non collectés
             max_distance = max([min([5000, distance]), max_distance])
             radians = angle * pi / 180.0
             x = distance * cos(radians)
             y = distance * sin(radians)
-            point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance *119))
+            point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
             lcd.set_at(point, pygame.Color(255, 255, 255))
     pygame.display.update()
 
-scan_data = [0]*360
+scan_data = [0] * 360
 
 lidar.connect()
 
 try:
-    print(lidar.get_info)
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-    process_data(scan_data)
-except KeyboardInterrupt:
-    print('Stoping.')
+        process_data(scan_data)
 
-lidar.stop()
-lidar.disconnect()
+        keys = pygame.key.get_pressed()
+        quit = pygame.event.get(pygame.QUIT)              
+        if quit or keys[pygame.K_ESCAPE] or keys[pygame.K_SPACE]:
+            raise KeyboardInterrupt  # Simuler une interruption au clavier
+
+except KeyboardInterrupt:
+    print('Arrêt.')
+
+finally:
+    lidar.stop()
+    lidar.disconnect()

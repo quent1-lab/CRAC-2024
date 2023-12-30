613da7041ce0b5c70820dcc8881f6bfaecfc4a5e
Quentin <Quent@Quent1_HP>
2023-12-31 00:01:36 +0100

    Delete unnecessary binary files

--- baseF303k8/.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/USBPhy_STM32.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/USBPhy_STM32.o: \
- /home/quentin/.platformio/packages/framework-mbed/targets/TARGET_STM/USBPhy_STM32.cpp \
- .pio/build/nucleo_f303k8/mbed_config.h
--- baseF303k8/.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/trng_api.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/trng_api.o: \
- /home/quentin/.platformio/packages/framework-mbed/targets/TARGET_STM/trng_api.c \
- .pio/build/nucleo_f303k8/mbed_config.h

f3949c74c2fc2b351227966dc498866cbfb325a7
Quentin <quentin.cunha@outlook.fr>
2023-12-31 00:01:36 +0100

    Delete unnecessary binary files

--- baseF303k8/.pio/build/nucleo_f303k8/FrameworkMbedplatform/mbed-trace/source/mbed_trace.d
+++
@@ -1,4 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedplatform/mbed-trace/source/mbed_trace.o: \
- /home/quentin/.platformio/packages/framework-mbed/platform/mbed-trace/source/mbed_trace.c \
- .pio/build/nucleo_f303k8/mbed_config.h \
- /home/quentin/.platformio/packages/framework-mbed/platform/mbed-trace/include/mbed-trace/mbed_trace.h
--- baseF303k8/.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/lp_ticker.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nucleo_f303k8/FrameworkMbedtargets/TARGET_STM/lp_ticker.o: \
- /home/quentin/.platformio/packages/framework-mbed/targets/TARGET_STM/lp_ticker.c \
- .pio/build/nucleo_f303k8/mbed_config.h

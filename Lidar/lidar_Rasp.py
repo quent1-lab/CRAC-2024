01f8a507eac4445e1d0ede7d98be3b90ceb192be
Quentin <quentin@quentin-linux.fr>
2024-01-20 00:01:18 +0100

    Update Lidar and WIFI code

--- Lidar/lidar_Rasp.py
+++ Lidar/lidar_Rasp.py
@@ -469,7 +469,7 @@ class LidarScanner:
                     for objet in self.objets:
                         trajectoire_actuel, trajectoire_adverse, trajectoire_evitement = self.trajectoires_anticipation(self.ROBOT, objet, 1.5, 0.1, 50)
                         #print(objet)
-                        client.update_lidar_object(objet)
+                        client.update_lidar_object(str(objet))
                     
             except RPLidarException as e:
                 # Code pour gérer RPLidarException

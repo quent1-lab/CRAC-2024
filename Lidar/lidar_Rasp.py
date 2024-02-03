0367ce6249ec44e4702ca99155edd307f09d4019
Quentin <quentin.cunha@outlook.fr>
2024-02-03 16:29:29 +0100

    Refactor LidarScanner class and remove unused code

--- Lidar/lidar_Rasp.py
+++ Lidar/lidar_Rasp.py
@@ -113,95 +113,12 @@ class ComCAN:
             self.disconnect()
             pass
 
-class Objet:
-    def __init__(self, id, x, y, taille):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.taille = taille
-        self.positions_precedentes = [(x, y, time.time())]  # Ajout du temps actuel
-        self.direction = 0
-        self.vitesse = 0
-        self.vitesse_ms = 0
-        self.points = []
-        self.last_moved = time.time()
-
-    def update_position(self, x, y):
-        # Mettre à jour la position de l'objet et ajouter la position précédente à la liste
-        if self.x != x or self.y != y:
-            self.positions_precedentes.append((self.x, self.y, time.monotonic_ns()))
-            self.x = x
-            self.y = y
-            self.last_moved = time.time()
-
-    def reset_if_not_moved(self, delay):
-        if time.time() > self.last_moved + delay:
-            return True
-        else:
-            return False
-
-    def get_direction_speed(self):
-        # Calculer le vecteur de déplacement entre la position actuelle et la position précédente
-        dx = self.x - self.positions_precedentes[-1][0]
-        dy = self.y - self.positions_precedentes[-1][1]
-
-        # Calculer le temps écoulé entre la position actuelle et la position précédente
-        dt = (time.monotonic_ns() - self.positions_precedentes[-1][2]) + 0.0000001 # Ajout de 0.00001 pour éviter la division par 0
-
-        # La direction est l'angle du vecteur de déplacement
-        self.direction = math.atan2(dy, dx)
-
-        # La vitesse est la magnitude du vecteur de déplacement divisée par le temps écoulé
-        self.vitesse = math.sqrt(dx**2 + dy**2) / dt * 1000000000  # Conversion en mm/s
-
-        # Convertir la vitesse en m/s
-        self.vitesse_ms = self.vitesse /1000
-
-        return self.direction, self.vitesse
-
-    def simulate_movement(self, direction, vitesse, temps):
-        # Convertir la direction en radians
-
-        # Calculer le déplacement en x et y
-        dx = vitesse * 10 * math.cos(direction) * temps
-        dy = vitesse * 10 * math.sin(direction) * temps
-
-        # Mettre à jour la position de l'objet
-        self.x += dx
-        self.y += dy
-
-    def calculate_dx_dy(self, direction, vitesse, temps):
-        # Assurez-vous que la direction est entre 0 et 2π
-        direction = direction % (2 * math.pi)
-
-        # Calculer le déplacement en x et y
-        dx = vitesse  * math.cos(direction) * temps
-        dy = vitesse  * math.sin(direction) * temps
-
-        return dx, dy
-
-    def __str__(self):
-        # Retourne une chaîne de caractères représentant l'objet sous format JSON
-        return f"{{\"id\": {self.id}, \"x\": {int(self.x)}, \"y\": {int(self.y)}, \"taille\": {int(self.taille)}}}"
-    
 class LidarScanner:
     def __init__(self, port=None):
         self.port = port
         self.lidar = None
-        self.lcd = None
         self.ROBOT_ANGLE = 0
-        self.BLACK = (0, 0, 0)
-        self.WHITE = (255, 255, 255)
-        self.LIGHT_GREY = (200, 200, 200)
-        self.GREEN = (0, 255, 0)
-        self.BLUE = (0, 0, 255)
-        self.RED = (255, 0, 0)
-        self.DARK_GREEN = (0, 100, 0)
-        self.FIELD_SIZE = (3000, 2000)
         self.BORDER_DISTANCE = 200
-        self.POINT_COLOR = (255, 0, 0)
-        self.BACKGROUND_COLOR = self.LIGHT_GREY
-        self.FONT_COLOR = self.BLACK
 
         # Initialisation du robot virtuel
         self.ROBOT = Objet(0, 1500, 1000, 20)

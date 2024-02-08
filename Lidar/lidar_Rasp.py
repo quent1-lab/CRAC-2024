d8adc56e4640663dd14850d1514a920e2e74d79e
Quentin <Quent@Quent1_HP>
2024-02-08 18:21:11 +0100

    Suppression de la classe ComCAN et de son utilisation dans le fichier lidar_Rasp.py

--- Lidar/lidar_Rasp.py
+++ Lidar/lidar_Rasp.py
@@ -6,11 +6,10 @@ import serial.tools.list_ports
 import os
 from objet import Objet
 import time
-import can
 import socket
 import threading
 import pickle
-
+from CAN import ComCAN
 
 class Client:
     def __init__(self):
@@ -59,59 +58,6 @@ class Client:
     def get_robot_angle(self):
         with self.Robot_lock:
             return self.ROBOT_angle
-            
-class ComCAN:
-    def __init__(self, channel, bustype):
-        self.channel = channel
-        self.bustype = bustype
-        self.can = None
-
-    def connect(self):
-        #Vérifie si le système d'exploitation est Linux
-        #Si oui, on lance les commandes pour configurer le CAN
-        try:
-            if os.name == "posix":
-                os.system('sudo ip link set can0 type can bitrate 1000000')
-                os.system('sudo ifconfig can0 up')
-                self.can = can.interface.Bus(channel = self.channel, bustype = self.bustype)
-            else:
-                logging.error("OS not supported")
-                raise OSError("OS not supported")       
-        except Exception as e:
-            logging.error(f"Failed to connect to CAN: {e}")
-            raise
-
-    def disconnect(self):
-        try:
-            self.can.shutdown()
-        except Exception as e:
-            logging.error(f"Failed to disconnect from CAN: {e}")
-            raise
-
-    def send(self, data):
-        try:
-            self.can.send(data)
-        except Exception as e:
-            logging.error(f"Failed to send data to CAN: {e}")
-            raise
-
-    def receive(self):
-        try:
-            return self.can.recv(10.0)
-        except Exception as e:
-            logging.error(f"Failed to receive data from CAN: {e}")
-            raise
-
-    def run(self):
-        try:
-            self.connect()
-            while True:
-                data = self.receive()
-                print(data)
-                
-        except KeyboardInterrupt:
-            self.disconnect()
-            pass
 
 class LidarScanner:
     def __init__(self, port=None):

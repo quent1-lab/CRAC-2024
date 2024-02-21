858ee96d84c3a0efc92178201e05df9eaa54fd8d
Quentin <quentin.cunha@outlook.fr>
2024-02-21 19:37:00 +0100

    Ajouter des messages de débogage pour le client ComWIFI

--- WIFI/client.py
+++ WIFI/client.py
@@ -6,8 +6,10 @@ COMWIFI_IP = '192.168.22.100'  # Adresse IP de la Raspberry Pi
 COMWIFI_PORT = 12345  # Port sur lequel le serveur ComWIFI écoute
 i = 0
 # Boucle d'envoi de données au serveur ComWIFI
+print("Client ComWIFI démarré")
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        print("Connexion au serveur ComWIFI...")
         client_socket.connect((COMWIFI_IP, COMWIFI_PORT))
         message = "programme client 1: " + str(i)
         i += 1

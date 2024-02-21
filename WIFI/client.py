39f83b3f2123eaaf5eb262ad69eca93da9474368
Quentin <quentin.cunha@outlook.fr>
2024-02-21 22:38:41 +0100

    Ajout de la gestion des threads pour l'envoi et la réception de données

--- WIFI/client.py
+++ WIFI/client.py
@@ -1,19 +1,40 @@
 import socket
 import time
+import threading
 
 # Configuration du client
 COMWIFI_IP = '192.168.22.100'  # Adresse IP de la Raspberry Pi
 COMWIFI_PORT = 22050  # Port sur lequel le serveur ComWIFI écoute
-i = 0
-# Boucle d'envoi de données au serveur ComWIFI
-print("Client ComWIFI démarré")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    print("Connexion au serveur ComWIFI...")
-    client_socket.connect((COMWIFI_IP, COMWIFI_PORT))
-    while True:
-        message = "programme client 2: " + str(i)
+stop_threads = True
+
+def receive_data(client_socket):
+    while stop_threads:
+        data_received = client_socket.recv(1024)
+        print("Données reçues du serveur ComWIFI:", data_received.decode())
+    
+def send_data(client_socket):
+    i = 0
+    while stop_threads:
+        message = "programme client 1: " + str(i)
         i += 1
         client_socket.sendall(message.encode())
         print("Données envoyées au serveur ComWIFI:", message)
         time.sleep(1)  # Attendre une seconde avant d'envoyer la prochaine donnée
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((COMWIFI_IP, COMWIFI_PORT))
+
+    receive_thread = threading.Thread(target=receive_data, args=(client_socket,))
+    send_thread = threading.Thread(target=send_data, args=(client_socket,))
+
+    receive_thread.start()
+    send_thread.start()
+
+    while True:
+        if input("Appuyez sur 'q' pour quitter\n") == 'q':
+            stop_threads = False
+            receive_thread.join()
+            send_thread.join()
+            break
+

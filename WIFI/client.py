7a8915411ddefe1f423b76e79ec2060c7aa324c8
Quentin <Quent@Quent1_HP>
2024-02-21 20:42:54 +0100

    Modifications de connexion au serveur ComWIFI dans le client.py

--- WIFI/client.py
+++ WIFI/client.py
@@ -7,10 +7,11 @@ COMWIFI_PORT = 22050  # Port sur lequel le serveur ComWIFI écoute
 i = 0
 # Boucle d'envoi de données au serveur ComWIFI
 print("Client ComWIFI démarré")
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        print("Connexion au serveur ComWIFI...")
-        client_socket.connect((COMWIFI_IP, COMWIFI_PORT))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    print("Connexion au serveur ComWIFI...")
+    client_socket.connect((COMWIFI_IP, COMWIFI_PORT))
+    while True:
         message = "programme client 1: " + str(i)
         i += 1
         client_socket.sendall(message.encode())

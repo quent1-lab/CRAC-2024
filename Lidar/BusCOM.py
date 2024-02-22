94f68fcda00c5d0c57d5b8db07ee403e37d30cc6
Quentin <quentin.cunha@outlook.fr>
2024-02-22 21:16:44 +0100

    Mise à jour de la variable stop_threads dans BusCOM.py

--- Lidar/BusCOM.py
+++ Lidar/BusCOM.py
@@ -9,13 +9,13 @@ PORT = 22050  # Port sur lequel le serveur écoute
 # Liste pour stocker tous les threads clients
 client_threads = []
 # Variable de contrôle pour arrêter les threads
-stop_threads = True
+stop_threads = False
 
 # Fonction pour gérer chaque client
 def handle_client(connection, address):
     global stop_threads
     print('Connecté à', address)
-    while stop_threads:
+    while not stop_threads:
         data = connection.recv(1024)  # Recevoir des données du client
         if not data:
             break  # Si les données sont vides, sortir de la boucle
@@ -26,7 +26,7 @@ def handle_client(connection, address):
 
 def handle_connection():
     global stop_threads, client_threads
-    while stop_threads:
+    while not stop_threads:
         try:
             connection, address = server_socket.accept()
             thread = threading.Thread(target=handle_client, args=(connection, address))
@@ -49,7 +49,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     connection_thread.start()
 
     # Boucle d'acceptation des connexions entrantes
-    while stop_threads:
+    while not stop_threads:
         if keyboard.is_pressed('space'):  # Si la touche espace est enfoncée
             stop_threads = False  # Indiquer aux threads de s'arrêter
             break

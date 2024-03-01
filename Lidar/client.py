3ce4d0552ac88ce1caf70051eac5e5dae560ab0e
Quentin <quentin@quentin-linux.fr>
2024-03-01 20:20:56 +0100

    Mise à jour de la fonction de validation des formulaires***

--- Lidar/client.py
+++ Lidar/client.py
@@ -2,9 +2,7 @@ import socket
 import time
 import threading
 import json
-
-class ClientException(Exception):
-    '''Classe d'exception de base pour le client'''
+from queue import Queue, Empty
 
 class Client:
     def __init__(self, _ip, _port, _id_client=2205, _callback=None, _test=False):
@@ -38,99 +36,84 @@ class Client:
     def receive_messages(self, socket):
         buffer = ""
         while not self.stop_threads:
-            data = socket.recv(4096)
-            if not data:
+            try:
+                data = socket.recv(4096)
+                if not data:
+                    break
+                buffer += data.decode()
+                while "\n" in buffer:
+                    line, buffer = buffer.split("\n", 1)
+                    yield line
+            except ConnectionResetError:
                 break
-            buffer += data.decode()
-            while "\n" in buffer:
-                line, buffer = buffer.split("\n", 1)
-                yield line
 
     def receive_task(self):
-        try:
-            while True:
-                with self.lock:
-                    if self.stop_threads:
-                        break
-                for _message in self.receive_messages(self.client_socket):
-                    for message in self.load_json(_message):
-                        self.callback(message)
-        except Exception as e:
-            raise ClientException(f"Erreur lors de la réception des messages : {e}")
+        while not self.stop_threads:
+            for _message in self.receive_messages(self.client_socket):
+                for message in self.load_json(_message):
+                    self.callback(message)
 
-    def send_data(self):
-        try:
-            i = 0
-            while True:
-                with self.lock:
-                    if self.stop_threads:
-                        break
-                message = self.create_message(1, "data", i)
-                i += 1
-                self.add_to_send_list(message)
-                print("Données envoyées au serveur ComWIFI:", message)
-                while True:
-                    with self.lock:
-                        if len(self.send_list) > 0 and not self.stop_threads:
-                            break
-                    time.sleep(0.5)
-        except Exception as e:
-            raise ClientException(f"Erreur lors de l'envoi des données : {e}")
-    
-    def send(self, message):
-        try:
-            messageJSON = json.dumps(message) + "\n"
-            with self.lock:
-                self.client_socket.sendall(messageJSON.encode())
-        except Exception as e:
-            raise ClientException(f"Erreur lors de l'envoi du message : {e}")
-    
     def send_task(self):
-        try:
-            while True:
-                with self.lock:
-                    if self.stop_threads:
-                        break
-                for message in self.send_list:
-                    self.send(message)
-                    self.send_list.remove(message)
-                    time.sleep(0.01)
-        except Exception as e:
-            raise ClientException(f"Erreur lors de l'envoi des tâches : {e}")
-    
-    def add_to_send_list(self, message):
-        with self.lock:
-            self.send_list.append(message)
+        while not self.stop_threads:
+            try:
+                message = self.send_queue.get(timeout=0.1)
+                self.send(message)
+            except Empty:
+                pass
 
-    def load_json(self, data):
-        try:
-            messages = []
-            for message in data.split('\n'):
-                if message:  # Ignore les lignes vides
-                    messages.append(json.loads(message))
-            return messages
-        except Exception as e:
-            raise ClientException(f"Erreur lors du chargement du JSON : {e}")
+    def add_to_send_list(self, message):
+        self.send_queue.put(message)
 
     def stop(self):
-        try:
-            if self.callback_stop is not None:
-                self.callback_stop()
+        if self.callback_stop is not None:
+            self.callback_stop()
+        self.stop_threads = True
+        self.send_queue.join()  # Attend la fin de la mise en file d'attente
+        close_thread = threading.Thread(target=self.close_connection)
+        close_thread.start()
+        print("CLIENT : Arrêt de la connexion pour le client", self.client_name)
+        close_thread.join()
+
+    def send_data(self):
+        i = 0
+        while True:
+            with self.lock:
+                if self.stop_threads:
+                    break
+            message = self.create_message(1, "data", i)
+            i += 1
+            self.add_to_send_list(message)
+            print("Données envoyées au serveur ComWIFI:", message)
+            while True:
+                with self.lock:
+                    if len(self.send_list) > 0 and not self.stop_threads:
+                        break
+                time.sleep(0.5)
+    
+    def send(self, message):
+        messageJSON = json.dumps(message) + "\n"
+        try :
+            with self.lock:
+                self.client_socket.sendall(messageJSON.encode())
+        except ConnectionResetError:
+            print("CLIENT : Erreur de connexion pour le client", self.client_name)
             self.stop_threads = True
-            close_trhead = threading.Thread(target=self.close_connection)
-            close_trhead.start()
-            print("Arrêt de la connexion pour le client", {self.id_client})
-        except Exception as e:
-            raise ClientException(f"Erreur lors de l'arrêt du client : {e}")
+    
+    def load_json(self, data):
+        messages = []
+        for message in data.split('\n'):
+            if message:  # Ignore les lignes vides
+                try :
+                    messages.append(json.loads(message))
+                except json.JSONDecodeError:
+                    print("CLIENT : Erreur de décodage JSON pour le client", self.client_name)
+        return messages
     
     def close_connection(self):
-        try:
-            for task in self.tasks:
-                task.join()
-            self.client_socket.close()
-            print("Connexion fermée pour le client", {self.id_client})
-        except Exception as e:
-            raise ClientException(f"Erreur lors de la fermeture de la connexion : {e}")
+        for task in self.tasks:
+            task.join()
+        self.client_socket.close()
+        print("CLIENT : Connexion fermée pour le client", self.client_name)
     
     def set_callback(self, _callback):
         self.callback = _callback
@@ -139,46 +122,37 @@ class Client:
         self.callback_stop = _callback
     
     def connect(self):
-        try:
-            print("Connexion du client", self.id_client, "au serveur ComWIFI")
-            i = 0
-            while True:
-                i += 1
-                try:
-                    self.client_socket.connect((self.ip, self.port))
-                    break  # Si la connexion est réussie, sortir de la boucle
-                except socket.error as e:
-                    print("Erreur de connexion pour le client", self.id_client, "au serveur ComWIFI")
-                    print("Réessai de la connexion dans 2 secondes")
-                    if i >= 3:
-                        print("Nombre maximum de tentatives de connexion atteint")
-                        raise e
-                    time.sleep(3)  # Attendre 3 secondes avant de réessayer
+        print("CLIENT : Connexion du client", self.client_name, "au serveur")
+        for i in range(3):
+            try:
+                self.client_socket.connect((self.ip, self.port))
+                break  # Si la connexion est réussie, sortir de la boucle
+            except socket.error as e:
+                print(f"CLIENT : Erreur de connexion pour",self.client_name,"au serveur")
+                time.sleep(2)  # Attendre 2 secondes avant de réessayer
+        else:
+            print("CLIENT : Nombre maximum de tentatives de connexion atteint")
+            raise e
 
-            if self.id_client is None:
-                self.id_client = 2205
-            message = self.create_message(1, "init", None)
-            self.send(message)
+        if self.id_client is None:
+            self.id_client = 2205
+        message = self.create_message(1, "init", None)
+        self.send(message)
 
-            receive_thread = threading.Thread(target=self.receive_task)
-            receive_thread.start()
+        receive_thread = threading.Thread(target=self.receive_task)
+        receive_thread.start()
 
-            if self.test:
-                send_thread = threading.Thread(target=self.send_data)
-                send_thread.start()
-            else:
-                send_thread = threading.Thread(target=self.send_task)
-                send_thread.start()
-            
-            self.tasks.append(receive_thread)
-            self.tasks.append(send_thread)
-        except Exception as e:
-            raise ClientException(f"Erreur lors de la connexion du client : {e}")
+        if self.test:
+            send_thread = threading.Thread(target=self.send_data)
+            send_thread.start()
+        else:
+            send_thread = threading.Thread(target=self.send_task)
+            send_thread.start()
+        
+        self.tasks.append(receive_thread)
+        self.tasks.append(send_thread)
 
 if __name__ == "__main__":
     # Utilisation de la classe
-    try:
-        client = Client('127.0.0.1', 22050)
-        client.connect()
-    except ClientException as e:
-        print(f"Erreur lors de l'utilisation du client : {e}")
+    client = Client('127.0.0.1', 22050)
+    client.connect()

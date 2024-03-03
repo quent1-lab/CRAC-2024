21ac505f0542a0e0f2bf7549ec5a1bdcaf6de0ac
Quentin <quentin.cunha@outlook.fr>
2024-03-03 21:41:41 +0100

    Ajouter la journalisation des événements du bus CAN et de l'IHM

--- Lidar/CAN.py
+++ Lidar/CAN.py
@@ -2,6 +2,10 @@ import can
 import os
 from client import Client
 import struct
+import logging
+
+# Configuration du logger
+logging.basicConfig(filename='buscan.log', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S', format='%(asctime)s - %(levelname)s - %(message)s')
 
 class ComCAN:
     def __init__(self, channel, bustype):
@@ -12,98 +16,101 @@ class ComCAN:
         self.client = Client("127.0.0.2", 22050, 2, self.disconnect)
 
     def connect(self):
-        #Vérifie si le système d'exploitation est Linux
-        #Si oui, on lance les commandes pour configurer le CAN
         try:
             if os.name == "posix":
                 os.system('sudo ip link set can0 type can bitrate 1000000')
                 os.system('sudo ifconfig can0 up')
-                self.can = can.interface.Bus(channel = self.channel, bustype = self.bustype)
+                self.can = can.interface.Bus(channel=self.channel, bustype=self.bustype)
                 self.is_connected = True
-                print("BusCAN : Connexion établie")
+                logging.info("BusCAN : Connexion établie")
             else:
-                print("BusCAN : Le système d'exploitation n'est pas compatible avec le CAN")
+                logging.error("BusCAN : Le système d'exploitation n'est pas compatible avec le CAN")
         except Exception as e:
-            pass
+            logging.error(f"Erreur lors de la connexion : {str(e)}")
 
     def disconnect(self):
         try:
             self.can.shutdown()
             self.is_connected = False
+            logging.info("BusCAN : Connexion fermée")
         except Exception as e:
-            pass
+            logging.error(f"Erreur lors de la déconnexion : {str(e)}")
 
     def send(self, data):
         try:
             self.can.send(data)
+            logging.info("Message envoyé sur le bus CAN")
         except Exception as e:
-            pass
+            logging.error(f"Erreur lors de l'envoi du message : {str(e)}")
     
     def packed(self, id, data):
-        # id : int, data : [(int, str)] -> [(data, format)]
         dataCan = None
-        for i in range(len(data)):
-            if dataCan is None:
-                dataCan = struct.pack(data[i][1], data[i][0])
-            else:
-                dataCan += struct.pack(data[i][1], data[i])
-        return can.Message(arbitration_id = id, data = dataCan, is_extended_id = False)
+        try:
+            for i in range(len(data)):
+                if dataCan is None:
+                    dataCan = struct.pack(data[i][1], data[i][0])
+                else:
+                    dataCan += struct.pack(data[i][1], data[i])
+        except Exception as e:
+            logging.error(f"Erreur lors de l'emballage des données : {str(e)}")
+        return can.Message(arbitration_id=id, data=dataCan, is_extended_id=False)
     
     def receive(self):
         try:
             messageCan = self.can.recv(10.0)
             if messageCan is not None:
+                logging.info("Message reçu sur le bus CAN")
                 return messageCan.arbitration_id, messageCan.dlc, messageCan.data
             else:
                 return None
         except Exception as e:
-            pass
+            logging.error(f"Erreur lors de la réception du message : {str(e)}")
     
     def analyse_CAN(self, data):
-        dataX = data[2]
-        if data[0] == 0x28:
-            # type data : bytearray(b'\x00\x00\x00\x00\x00\x00') -> 0-2 : (int)x, 2-4 : (int)y, 4-6 : (in)theta 
-            x = struct.unpack('h', dataX[0:2])
-            y = struct.unpack('h', dataX[2:4])
-            theta = struct.unpack('h', dataX[4:6])
-            #print("BusCAN : x = ", x, "y = ", y, "theta = ", theta)
-            self.client.add_to_send_list(self.client.create_message(0, "coord", {"x": x[0], "y": y[0], "theta": theta[0]}))
-        else:
-            print("BusCAN : ID inconnu ; data :",data)
+        try:
+            dataX = data[2]
+            if data[0] == 0x28:
+                x = struct.unpack('h', dataX[0:2])
+                y = struct.unpack('h', dataX[2:4])
+                theta = struct.unpack('h', dataX[4:6])
+                self.client.add_to_send_list(self.client.create_message(0, "coord", {"x": x[0], "y": y[0], "theta": theta[0]}))
+            else:
+                logging.error(f"ID inconnu ; data : {data}")
+        except Exception as e:
+            logging.error(f"Erreur lors de l'analyse du message CAN : {str(e)}")
 
     def receive_to_server(self, message):
-        if message["cmd"] == "stop":
-            self.client.stop()
-        elif message["cmd"] == "data":
-            messageCAN = message["data"]
-            self.send(messageCAN)
-        elif message["cmd"] == "clic":
-            # type data à envoyer : short x, short y, short theta, char sens
-            data = message["data"]
-            dataCan = struct.pack('h', data["x"]) + struct.pack('h', data["y"]) + struct.pack('h', data["theta"]) + struct.pack('c', data["sens"].encode())
-            messageCan = can.Message(arbitration_id = 0x20, data = dataCan, is_extended_id = False)
-            self.send(messageCan)
-        elif message["cmd"] == "recal":
-            data = message["data"]
-            dataCan = struct.pack('h', data["x"]) + struct.pack('h', data["y"]) + struct.pack('h', data["theta"])
-            messageCan = can.Message(arbitration_id = 0x22, data = dataCan, is_extended_id = False)
-            self.send(messageCan)
+        try:
+            if message["cmd"] == "stop":
+                self.client.stop()
+            elif message["cmd"] == "data":
+                messageCAN = message["data"]
+                self.send(messageCAN)
+            elif message["cmd"] == "clic":
+                data = message["data"]
+                dataCan = struct.pack('h', data["x"]) + struct.pack('h', data["y"]) + struct.pack('h', data["theta"]) + struct.pack('c', data["sens"].encode())
+                messageCan = can.Message(arbitration_id=0x20, data=dataCan, is_extended_id=False)
+                self.send(messageCan)
+            elif message["cmd"] == "recal":
+                data = message["data"]
+                dataCan = struct.pack('h', data["x"]) + struct.pack('h', data["y"]) + struct.pack('h', data["theta"])
+                messageCan = can.Message(arbitration_id=0x22, data=dataCan, is_extended_id=False)
+                self.send(messageCan)
+        except Exception as e:
+            logging.error(f"Erreur lors du traitement du message serveur : {str(e)}")
 
 
     def run(self):
-        self.client.set_callback_stop(self.disconnect)
-        self.client.set_callback(self.receive_to_server)
         try:
+            self.client.set_callback_stop(self.disconnect)
+            self.client.set_callback(self.receive_to_server)
             self.client.connect()
             self.connect()
             while self.is_connected:
                 data = self.receive()
-
                 if data is not None:
                     self.analyse_CAN(data)
-            
             exit(0)
-                
         except KeyboardInterrupt:
             self.client.send(self.client.create_message(0, "stop", {}))
             pass

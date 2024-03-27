3d7ed5efe79a1d9e88dceac6949ff62370509f5b
Quentin_Crac <quentin.cunha@outlook.fr>
2024-03-27 19:53:09 +0100

    Update DISPLAY environment variable value

--- src/IHM_Robot.py
+++ src/IHM_Robot.py
@@ -194,7 +194,7 @@ bouton = pygame.Rect(position_bouton, taille_bouton)
 couleur_fond = BLANC
 
 if 'DISPLAY' not in os.environ:
-    os.environ['DISPLAY'] = ':1'
+    os.environ['DISPLAY'] = ':0'
 
 # Boucle principale
 while True:

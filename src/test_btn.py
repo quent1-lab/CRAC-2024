74267feb08c1ac82421902b118a5a02ad64957b0
Quentin_Crac <quentin.cunha@outlook.fr>
2024-04-28 17:39:53 +0200

    Mise à jour de la gestion du bouton dans test_btn.py

--- src/test_btn.py
+++ src/test_btn.py
@@ -1,17 +1,12 @@
-import RPi.GPIO as GPIO
 import time
+import gpiozero
 
-# Utilisez la numérotation des broches du BCM
-GPIO.setmode(GPIO.BOARD)
+button = gpiozero.Button(16)
 
-# Définissez le pin 18 comme une entrée et activez la résistance de pull-up interne
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-while True:
-    # Si le bouton est appuyé (c'est-à-dire que l'entrée est faible)
-    if GPIO.input(16) == GPIO.LOW:
-        print("Bouton appuyé!")
-        time.sleep(0.2)  # Debounce time
-
-# N'oubliez pas de nettoyer à la fin de votre script
-GPIO.cleanup()
+if __name__ == '__main__':
+    while True:
+        if button.is_pressed:
+            print("Button is pressed")
+        else:
+            print("Button is not pressed")
+        time.sleep(0.5)

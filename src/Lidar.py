2f78f09dbe9606a4d247ba01202499c8dc34a724
Quentin <quentin.cunha@outlook.fr>
2024-04-29 23:49:53 +0200

    Mise à jour des positions de départ et de l'équipe dans IHM.py et Lidar.py

--- src/Lidar.py
+++ src/Lidar.py
@@ -297,7 +297,7 @@ class LidarScanner:
 
 if __name__ == '__main__':
     # Initialiser le client
-    scanner = LidarScanner("/dev/ttyUSB0")
+    scanner = LidarScanner()
     try :
         print("LIDAR  : Démarrage du programme")
         logging.info("Starting LiDAR scanner")

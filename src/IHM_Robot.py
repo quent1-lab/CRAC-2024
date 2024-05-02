797e12767cab491658f8d5b4b52dd52156f418f4
QuentinLinux <quentin.cunha@outlook.fr>
2024-05-02 19:00:19 +0200

    Mise à jour des coordonnées et actions dans strategie.json

--- src/IHM_Robot.py
+++ src/IHM_Robot.py
@@ -295,8 +295,8 @@ class IHM_Robot:
                         logging.info(f"Commande : {commande}")
                         # Envoyer les commandes au CAN
                         for i, cmd in enumerate(commande):
-                            self.client.add_to_send_list(self.client.create_message(2, "CAN", {"id": 0x1A0, "byte1": cmd, "byte2": 0, "byte3": 0}))
-                            time.sleep(5)
+                            self.client.add_to_send_list(self.client.create_message(2, "CAN", {"id": 0x1A0, "byte1": cmd}))
+                            time.sleep(2)
                             """while not self.robot_move and self.strategie_is_running and self.is_running:
                                 time.sleep(0.1)
                                 if aknowledge[i] in self.liste_aknowledge:

b89f4b2fb3eaba8d3a3c91a3fb682a7b34704532
QuentinLinux <quentin.cunha@outlook.fr>
2024-05-05 18:47:59 +0200

    chore: Update LidarScanner class to adjust angle calculation
    ```

--- src/Lidar.py
+++ src/Lidar.py
@@ -42,7 +42,7 @@ class LidarScanner:
         points = []
         for point in scan:
             distance = point[2]
-            new_angle = point[1] - self.ROBOT_ANGLE
+            new_angle = point[1] - self.ROBOT_ANGLE + 180
             
             x_r = self.map_value(self.ROBOT.x, 0, 3000, 3000, 0)
 

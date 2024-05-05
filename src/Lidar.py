603845e4ec46a3e1d130df1344fb3e5c8118fc64
Quentin_Crac <quentin.cunha@outlook.fr>
2024-05-05 11:38:29 +0200

    chore: Update LidarScanner class to include robot coordinates in angle calculation

--- src/Lidar.py
+++ src/Lidar.py
@@ -40,6 +40,9 @@ class LidarScanner:
         for point in scan:
             distance = point[2]
             new_angle = point[1] - self.ROBOT_ANGLE
+            
+            x_r = self.map_value(self.ROBOT.x, 0, 3000, 3000, 0)
+            y_r = self.map_value(self.ROBOT.y, 0, 2000, 2000, 0)
 
             new_angle %= 360
             if new_angle < 0:

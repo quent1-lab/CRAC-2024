aba233b89990fde900fddbf4270350825f5d0619
QuentinLinux <quentin.cunha@outlook.fr>
2024-05-08 02:27:22 +0200

    chore: Update Lidar.py to filter out points with significantly different distances from neighboring points

--- src/Lidar.py
+++ src/Lidar.py
@@ -53,7 +53,8 @@ class LidarScanner:
                 continue
             
             # Filtre les points qui ne se suivent pas à -1 et 1 degré
-            if i > 0 and abs(scan[i-1][1] - point[1]) > 1:
+            # et les points dont la distance est significativement différente de celle des points voisins
+            if i > 0 and (abs(scan[i-1][1] - point[1]) > 1 or abs(scan[i-1][0] - point[0]) > 30):
                 continue
             
             distance = point[2]
